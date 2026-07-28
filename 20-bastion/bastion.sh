c2b2d0284019eb909ac29eaec42253141c060e9f
Blujaytech <blujaytechnlogies@gmail.com>
2026-07-28 17:36:42 +0530

    roboshop-infra

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -1,72 +1,37 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+growpart /dev/nvme0n1 4
+lvextend -L +30G /dev/RootVG/varVol
+xfs_growfs /var
 
-ARCH=amd64
-PLATFORM=$(uname -s)_$ARCH
-
-VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2...$G SUCCESS $N"
-    fi
-}
-
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
-fi
-
-dnf install mysql -y
-VALIDATE $? "Install MySQL"
-# docker
-yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# Docker
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
-VALIDATE $? "Docker installation"
 
 # eksctl
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-mv /tmp/eksctl /usr/local/bin
-eksctl version
-VALIDATE $? "eksctl installation"
-
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz"
+tar -xzf eksctl_Linux_amd64.tar.gz -C /tmp && rm eksctl_Linux_amd64.tar.gz
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
 # kubectl
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.2/2026-02-27/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mv kubectl /usr/local/bin/kubectl
-VALIDATE $? "kubectl installation"
+cp kubectl /usr/local/bin/kubectl
 
-# kubens
-git clone https://github.com/ahmetb/kubectx /opt/kubectx
-ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-VALIDATE $? "kubens installation"
+# kubens (part of kubectx)
+curl -sLo /usr/local/bin/kubens https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens
+chmod +x /usr/local/bin/kubens
 
+# k9s
+curl -sLO "https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_amd64.tar.gz"
+tar -xzf k9s_Linux_amd64.tar.gz -C /tmp && rm k9s_Linux_amd64.tar.gz
+sudo install -m 0755 /tmp/k9s /usr/local/bin && rm /tmp/k9s
 
-#Helm
+# helm3
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-VALIDATE $? "helm installation"
-
-#k9S
-curl -sS https://webinstall.dev/k9s | bash
-VALIDATE $? "K9S installation"
--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 6.37.0, < 7.0.0"
+      version = "6.33.0" # Terraform AWS provider version
     }
   }
 
   backend "s3" {
-    bucket = "81s-remote-state-s3-prod"
-    key    = "roboshop-bastion"
-    region = "us-east-1"
-    dynamodb_table = "81s-locking-prod-new"
+    bucket  = "81s-remote-state-s3-prod" # Replace with your unique bucket name
+    key     = "terraform-eks-bastion"
+    region  = "us-east-1"
+    encrypt = true
+    use_lockfile   = true
   }
 }
 
 provider "aws" {
-  # Configuration options
   region = "us-east-1"
 }
